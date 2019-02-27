71a8273cf4fd8e176bed353047ca958816a04a39
Kevin van der Poll <vdpkevin@gmail.com>
2019-02-27 18:36:26 -0500

    Add react-qr-reader and kevin_qr.png

--- src/App.js
+++ src/App.js
@@ -1,28 +1,51 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import QrReader from "react-qr-reader";
+import "./App.css";
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+	state = {
+		// result: "No Result"
+		dataObject: "null"
+	};
+
+	handleScan = data => {
+		console.log(JSON.parse(data))
+		if (data) {
+			let dataObject = JSON.parse(data)
+			this.setState({ dataObject });
+		}
+		console.log('state', this.state)
+	};
+
+	handleError = err => {
+		console.log("there was an error", err);
+	};
+	render() {
+		return (
+			<div className="App">
+				<header className="App-header">
+					<p>
+						QR SCANNER
+					</p>
+					<QrReader
+						delay={1000}
+						onError={this.handleError}
+						onScan={this.handleScan}
+						style={{ width: "40%" }}
+					/>
+					{/* <p>{this.state.result}</p> */}
+					<p>ID: {this.state.dataObject.id}</p>
+					<p>First Name: {this.state.dataObject.first_name}</p>
+					<p>Last Name: {this.state.dataObject.last_name}</p>
+					<a href={this.state.dataObject.twitter}>Twitter</a>
+					<a href={this.state.dataObject.instagram}>Instagram</a>
+					<a href={this.state.dataObject.github}>Github</a>
+				</header>
+				
+			</div>
+		);
+	}
 }
 
 export default App;
